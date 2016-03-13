953f924585ed80b7b96dbf782bb2fab1662fa35a
Łukasz Zajączkowski <ukasz Zajczkowski>
2016-03-13 09:59:44 +0800

    KISS pt1

--- app/src/main/java/com/example/lukasz/whozzup/Create.java
+++ app/src/main/java/com/example/lukasz/whozzup/Create.java
@@ -31,25 +31,7 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Create.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Create#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Create extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     private Util util;
     private TextView response;
     ProgressDialog dialog;
@@ -103,65 +85,8 @@ public class Create extends Fragment {
             }
         });
 
-        final EditText editText3=(EditText)getView().findViewById(R.id.editText3);
-        editText3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final EditText input = new EditText(v.getContext());
-                input.setHeight(200);
-                new AlertDialog.Builder(v.getContext())
-                        .setTitle("Description")
-                        .setMessage("Write something about your event!")
-                        .setView(input)
-                        .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int whichButton) {
-                                editText3.setText(input.getText().toString());
-                            }
-                        })
-                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int whichButton) {
-                            }
-                        })
-                        .show();
-            }
-        });
-
-
-
-
     }
 
-
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Create.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Create newInstance(String param1, String param2) {
-        Create fragment = new Create();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-
-        }
-        util = new Util();
-    }
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
@@ -192,8 +117,12 @@ public class Create extends Fragment {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
-        final EditText maps = (EditText) v.findViewById(R.id.form_location);
 
+        Intent i = new Intent(getActivity(), MapsActivity.class);
+        startActivityForResult(i, 1);
+
+
+        final EditText maps = (EditText) v.findViewById(R.id.form_location);
         maps.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
@@ -206,21 +135,15 @@ public class Create extends Fragment {
 
         });
 
-
-
             final Button button = (Button) v.findViewById(R.id.CreateEventButton);
         response = (TextView) v.findViewById(R.id.ResponseText);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 EditText mEdit;
-//                mEdit = (EditText) getView().findViewById(R.id.editText);
                 String category = (String) spinner.getSelectedItem();
 
-                mEdit = (EditText) getView().findViewById(R.id.editText2);
+                mEdit = (EditText) getView().findViewById(R.id.form_title);
                 String title = mEdit.getText().toString();
-
-                mEdit = (EditText) getView().findViewById(R.id.editText3);
-                String description = mEdit.getText().toString();
 
                 mEdit = (EditText) getView().findViewById(R.id.form_location);
                 String location = mEdit.getText().toString();
@@ -231,7 +154,7 @@ public class Create extends Fragment {
                 mEdit = (EditText) getView().findViewById(R.id.editText7);
                 String time = mEdit.getText().toString();
 
-                new CreateEvent().execute("https://protected-ocean-61024.herokuapp.com/event/create/", category, title, description, location, date, time);
+                new CreateEvent().execute("https://protected-ocean-61024.herokuapp.com/event/create/", category, title, location, date, time);
 
                 dialog = new ProgressDialog(getView().getContext());
                 dialog.setCancelable(false);
@@ -263,10 +186,9 @@ public class Create extends Fragment {
                 info.put("creator", id);
                 info.put("category", str[1]);
                 info.put("title", str[2]);
-                info.put("description", str[3]);
-                info.put("location", str[4]);
-                info.put("date", str[5]);
-                info.put("time", str[6]);
+                info.put("location", str[3]);
+                info.put("date", str[4]);
+                info.put("time", str[5]);
 
                 JSONArray attendees = new JSONArray();
                 attendees.put(id);
@@ -298,14 +220,6 @@ public class Create extends Fragment {
         }
     }
 
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -322,19 +236,7 @@ public class Create extends Fragment {
         super.onDetach();
         mListener = null;
     }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 }
