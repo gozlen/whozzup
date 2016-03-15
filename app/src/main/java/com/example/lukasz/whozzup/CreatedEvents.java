708635192980144cb43ad78e24e95cdd7ea0df39
kaushal-silva <kaushal@connect.hku.hk>
2016-03-16 07:19:35 +0800

    All events clickable, work around for fragment to activity - use back functionality

--- app/src/main/java/com/example/lukasz/whozzup/CreatedEvents.java
+++ app/src/main/java/com/example/lukasz/whozzup/CreatedEvents.java
@@ -11,7 +11,11 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.facebook.AccessToken;
@@ -30,15 +34,60 @@ import java.util.List;
 public class CreatedEvents extends ListFragment {
 
     AsyncTask mTask;
-    List<Event> eventlist;
+    Button back;
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //Set a linearLayout to add buttons
+        RelativeLayout rl = new RelativeLayout(getActivity());
+        // Set the layout full width, full height
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams
+                (
+                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.MATCH_PARENT);
+        rl.setLayoutParams(params);
+
+
+        back = new Button(getActivity());
+        back.setText("Back");
+                //For buttons visibility, you must set the layout params in order to give some width and height:
+        params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+       params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+
+        back.setLayoutParams(params);
+
+        ViewGroup viewGroup = (ViewGroup) view;
+        rl.addView(back);
+        viewGroup.addView(rl);
+    }
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        //mTask = new allEvents().execute("https://protected-ocean-61024.herokuapp.com/event/all/");
+
+
+
         String id = id = AccessToken.getCurrentAccessToken().getUserId();
-        System.out.println("THE USER ID RIGHT NOW IS " + id + "\n");
         mTask = new eventsCreated().execute("https://protected-ocean-61024.herokuapp.com/user/events/", id);
 
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getFragmentManager().popBackStack();
+            }
+        });
+    }
+
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Event f = (Event) l.getItemAtPosition(position);
+        Intent i = new Intent(getActivity(), EventActivity.class);
+        i.putExtra("id", f.getId());
+        startActivity(i);
     }
 
 
@@ -50,53 +99,7 @@ public class CreatedEvents extends ListFragment {
 
 
 
-    private class allEvents extends AsyncTask<String, Void, List<Event>> {
-        protected List<Event> doInBackground(String... str){
-            InputStream in = null;
-            try {
-                DataOutputStream printout;
-                URL url = new URL(str[0]);
-                HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                con.setDoOutput(true);
-                con.setDoInput(true);
-                con.setRequestProperty("Content-Type", "application/json");
-                con.setRequestProperty("Accept", "application/json");
-                con.setRequestMethod("POST");
 
-                String id = AccessToken.getCurrentAccessToken().getUserId();
-                JSONObject info = new JSONObject();
-                info.put("userID", id);
-
-
-
-                printout = new DataOutputStream(con.getOutputStream ());
-                String data = info.toString();
-                byte[] send = data.getBytes("UTF-8");
-                printout.write(send);
-                printout.flush();
-                printout.close();
-                Util util = new Util();
-                in = con.getInputStream();
-                //System.out.println("WTF");
-
-                List<Event> eventList = util.readEventArray(in);
-
-                return eventList;
-
-
-            } catch (Exception e) {
-                return new ArrayList<>();
-
-            }
-        }
-
-        protected void onPostExecute(List<Event> eventList){
-            String id = AccessToken.getCurrentAccessToken().getUserId();
-
-            EventListAdapter customAdapter = new EventListAdapter(getActivity(), R.layout.event_list_item, eventList);
-            setListAdapter(customAdapter);
-        }
-    }
     private class eventsCreated extends AsyncTask<String, Void, List<Event>> {
 
         //private class eventsCreated extends AsyncTask<String, Void, String> {
